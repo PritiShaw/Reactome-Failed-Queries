31c3cd8f95a4903cf547cf954464f8ab044eaa9e
PritiShaw <pritishaw0103@gmail.com>
2020-07-12 19:47:47 +0530

    Batch1

--- run2.sh
+++ run2.sh
@@ -1,8 +1,8 @@
 python3 mergeOutputs.py /mnt/e/GSoC20/Reactome-Failed-Queries/eutils_output.csv /mnt/e/GSoC20/Reactome-Failed-Queries/text.out
 mkdir Output_Batch1
-mv pmid-list.txt Output_Batch1
+mv pmid_list.txt Output_Batch1
 mv eutils_output.csv Output_Batch1
-mv abstracts.txts Output_Batch1
+mv abstract.txts Output_Batch1
 mv text.out Output_Batch1
 git add .
 git commit -m "Batch 1"
